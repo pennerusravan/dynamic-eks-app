7d9d807267f10eb801c7d4321c304bc661e517ef
pennerusravan <sravankumarpenneru344@gmail.com>
2026-04-22 22:52:29 +0530

    Delete dynamic-eks-app directory

--- dynamic-eks-app/app/config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-SERVICE_NAME = os.getenv("SERVICE_NAME", "default-service")
-ENV = os.getenv("ENV", "dev")
-CUSTOM_VALUE = os.getenv("CUSTOM_VALUE", "none")
--- dynamic-eks-app/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "Starting application..."
-echo "SERVICE_NAME=$SERVICE_NAME"
-echo "ENV=$ENV"
-echo "CUSTOM_VALUE=$CUSTOM_VALUE"
-
-exec uvicorn app.main:app --host 0.0.0.0 --port 8000
